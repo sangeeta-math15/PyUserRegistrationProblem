4095cd4f33d5d2f8e5e2ec47cbf2acc75291c830
sngtmth <sngtmth@gmail.com>
2021-12-28 01:26:50 +0530

    Added:To check first name should start with capital letter it has min 3 characters

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,10 +1,7 @@
 from user_registration import UserRegistration
 import pytest
 
-def test_first_name():
+
+def test_first_name_when_proper_should_return_true():
     u_reg = UserRegistration()
     assert u_reg.first_name_validation("Sangeeta") == True or u_reg.first_name_validation("sangeeta") == False or u_reg.first_name_validation("SANGEETA") == False
-
-
-
-
