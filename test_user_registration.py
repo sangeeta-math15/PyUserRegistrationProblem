a30c0b524c3c52179b03a243aa4b709d1f6a035f
sngtmth <sngtmth@gmail.com>
2021-12-29 12:03:37 +0530

    Added:check vaid phone number

--- test_user_registration.py
+++ test_user_registration.py
@@ -8,24 +8,12 @@ def test_first_name_when_proper_should_return():
     assert u_reg.first_name_validation("Sangeeta") == True or u_reg.first_name_validation("sangeeta") == False
 
 
-# def test_first_name_when_improper_should_return_false():
-#     assert u_reg.first_name_validation("sangeeta") == False
-
-
 def test_last_name_when_proper_should_return():
     assert u_reg.last_name_validation("Math") == True or u_reg.last_name_validation("math") == False
 
 
-# def test_last_name_when_improper_should_return_false():
-#     assert u_reg.last_name_validation("math") == False
-
-
 def test_email_when_proper_should_return():
     assert u_reg.email_validation("sangeeta.1rn18mca30@gmail.com") == True or u_reg.email_validation("sangeeta.gmail.com") == False
-
-
-# def test_email_when_impproper_should_return_false():
-#     assert u_reg.email_validation("sangeeta.gmail.com") == False
 
 
 def test_phone_number_when_proper_should_return():
