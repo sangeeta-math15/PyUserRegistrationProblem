b3267e4556359d49cd9e22e38148f52dabd8d649
sngtmth <sngtmth@gmail.com>
2021-12-31 21:39:18 +0530

    Added:Check Valid Password

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,28 +1,40 @@
 from user_registration import UserRegistration
+
 import pytest
 
 u_reg = UserRegistration()
 
 
-def test_first_name_when_proper_should_return_happy():
-    assert u_reg.first_name_validation("Sangeeta") == "Happy"
+def test_first_name():
+    """
+    :return: true for valid first name otherwise false
+    """
+    assert u_reg.first_name_validation("Sangeeta") == True or u_reg.first_name_validation("sangeeta") == False
 
 
-def test_first_name_when_proper_should_return_sad():
-    assert u_reg.first_name_validation("sangeeta") == "Sad"
+def test_last_name():
+    """
+    :return: true for valid last name otherwise false
+    """
+    assert u_reg.last_name_validation("Math") == True or u_reg.last_name_validation("math") == False
 
 
-def test_last_name_when_proper_should_return_happy():
-    assert u_reg.last_name_validation("Math") == "Happy"
+def test_email():
+    """
+    :return: true for email validation otherwise false
+    """
+    assert u_reg.email_validation("sangeeta.1rn18mca30@gmail.com") == True or u_reg.email_validation("sangeeta.gmail.com") == False
 
 
-def test_last_name_when_proper_should_return_sad():
-    assert u_reg.last_name_validation("math") == "Sad"
+def test_phone_number():
+    """
+    :return: true for phone number validation otherwise false
+    """
+    assert u_reg.phone_number_validation("91 9422484996") == True or u_reg.phone_number_validation("9422484996") == False
 
 
-def test_email_when_proper_should_return_happy():
-    assert u_reg.email_validation("sangeeta.1rn18mca30@gmail.com") == "Happy"
-
-
-def test_email_when_proper_should_return_sad():
-    assert u_reg.email_validation("sangeeta.gmail.com") == "Sad"
+def test_password():
+    """
+    :return: true for password validation correct otherwise false
+    """
+    assert u_reg.password_validation("Sang@1234") == True or u_reg.password_validation("sang@math") == False
--- user_registration.py
+++ user_registration.py
@@ -1,15 +1,23 @@
-"""
-    re specifies a set of strings that matches it; the functions
-    in this module let you check if a particular string matches a given regular expression
-"""
 import re
+
+from user_registrationException import InputError
+
+"""
+    re specifies a set of strings that matches it; the functions in this module let you check 
+    if a particular string matches a given regular expression
+"""
 
 FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$"
 LAST_NAME = "^[A-Z]{1}[a-z]{2,}$"
 EMAIL_ADDRESS = "^[a-zA-z]{3}[0-9a-zA-Z\\.\\_\\-\\+]*@[a-z]*\\.(co|com.au|in|net|in|com.com|com|)$"
+PHONE_NUMBER = "^[0-9]{2} [0-9]{10}$"
+PASSWORD = "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=]).{8,20}$"
 
 
 class UserRegistration:
+    """
+    create class for validating user registration
+    """
 
     def __init__(self):
         pass
@@ -20,43 +28,85 @@ class UserRegistration:
         :param fname_input: user input
         :return: true for valid username
         """
+
         if re.match(FIRST_NAME, fname_input):
-            print("Valid first name")
-            return "Happy"
-        else:
-            print("Invalid first name ")
-            return "Sad"
+            return True
 
-    def last_name_validation(self,last_name_input):
+    def last_name_validation(self, lname_input):
         """
-
         :param last_name_input: user_input
         :return: true for valid user last name
         """
-        if re.match(LAST_NAME, last_name_input):
-            print("Valid last name")
-            return "Happy"
-        else:
-            print("Invalid last name")
-            return "Sad"
+        if re.match(LAST_NAME, lname_input):
+            return True
 
     def email_validation(self, email_input):
+        """
+        :param email_input:
+        :return:
+        """
         if re.match(EMAIL_ADDRESS, email_input):
-            print("Valid Email")
-            return "Happy"
-        else:
-            print("Invalid Email ")
-            return "Sad"
+            return True
+
+    def phone_number_validation(self, phone_number_input):
+        """
+        :param phone_number_input:
+        :return:
+        """
+        if re.match(PHONE_NUMBER, phone_number_input):
+            return True
+
+    def password_validation(self, password_input):
+        """
+        :param password_input:
+        :return:
+        """
+        if re.match(PASSWORD, password_input):
+            return True
+
 
 if __name__ == '__main__':
+
     user_reg = UserRegistration()
 
-    first_name_input = input("Enter First Name:")
-    user_reg.first_name_validation(first_name_input)
+    fname_input = input("Enter First Name:")
+    matched_input = user_reg.first_name_validation(fname_input)
 
-    last_name_input = input("Enter Last Name:")
-    user_reg.last_name_validation(last_name_input)
+    try:
+        if not matched_input:
+            raise InputError(" Enter the first name with minimum three characters ")
 
-    emial_input = input("Enter email address")
-    user_reg.email_validation(emial_input)
+    except InputError as e:
+        print(str(e))
 
+    lname_input = input("Enter Last Name:")
+    matched_input = user_reg.last_name_validation(lname_input)
+    try:
+        if not matched_input:
+            raise InputError(" Enter the last name with minimum three characters ")
+    except InputError as e:
+        print(str(e))
+
+    emial_input = input("Enter email address:")
+    matched_input = user_reg.email_validation(emial_input)
+    try:
+        if not matched_input:
+            raise InputError("Enter the valid email")
+    except InputError as e:
+        print(str(e))
+
+    phone_number_input = input("Enter phone number:")
+    matched_input = user_reg.phone_number_validation(phone_number_input)
+    try:
+        if not matched_input:
+            raise InputError("Enter the valid phone number")
+    except InputError as e:
+        print(str(e))
+
+    password_input = input("Enter password:")
+    matched_input = user_reg.password_validation(password_input)
+    try:
+        if not matched_input:
+            raise InputError("Enter a valid password with one capital letter, special character and a number")
+    except InputError as e:
+        print(str(e))
